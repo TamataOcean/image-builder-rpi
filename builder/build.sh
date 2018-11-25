b36c409dee795adb62345f5d7c438dc7dc1cf40c
ElRouqin <rtourte@yahoo.fr>
2018-11-25 15:03:01 +0100

    Update build.sh with Hypriot version

--- builder/build.sh
+++ builder/build.sh
@@ -26,7 +26,7 @@ echo CIRCLE_TAG="${CIRCLE_TAG}"
 
 # name of the sd-image we gonna create
 HYPRIOT_IMAGE_VERSION=${VERSION:="dirty"}
-HYPRIOT_IMAGE_NAME="hypriotos-rpi-${HYPRIOT_IMAGE_VERSION}.img"
+HYPRIOT_IMAGE_NAME="TamataWorkshop-${HYPRIOT_IMAGE_VERSION}.img"
 export HYPRIOT_IMAGE_VERSION
 
 # create build directory for assembling our image filesystem
